2208bdbf406e4d533ea209c29a22d657bfb76560
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2021-06-19 15:47:47 +0900

    employed text.slice

--- 00_intro/SimpleSearch.scala
+++ 00_intro/SimpleSearch.scala
@@ -1,7 +1,16 @@
 object SimpleSearch extends App {
     val text = "bvaindabcvjiasuianvaomravranvfavabcniwu".toSeq
     val pattern = "abc".toSeq
-    val matchIndexes = Seq()
+    val matchIndexes = search(text, pattern)
+
+    def search(text: Seq[Char], pattern: Seq[Char]): Seq[Int] = {
+        var matchIndexes = Seq[Int]()
+        for (i <- 0 to text.length - 1) {
+            val textSliced = text.slice(i, i + pattern.length)
+            println(textSliced)
+        }
+        matchIndexes
+    }
 
     println(s"found at ${matchIndexes}")
 }
