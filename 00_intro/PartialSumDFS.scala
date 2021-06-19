461a8626e74db520c25276cd365b53a38b44ee55
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2021-06-19 18:02:06 +0900

    added a  recursive implementation

--- 00_intro/PartialSumDFS.scala
+++ 00_intro/PartialSumDFS.scala
@@ -3,6 +3,17 @@ object PartialSumDFS extends App {
     val n = a.length
     val k = 8
 
-    val (isMatch, result) = (false, Seq())
+    def isMatchAndResult(index: Int, partial: Seq[Int]): (Boolean, Seq[Int]) = {
+        if (index == n) {
+            return if (partial.sum == k) (true, partial) else (false, Seq())
+        }
+        val (isMatchNotAdd, resultNotAdd) = isMatchAndResult(index + 1, partial)
+        if (isMatchNotAdd) return (true, resultNotAdd)
+
+        isMatchAndResult(index + 1, partial :+ a(index))
+    }
+
+    val (isMatch, result) = isMatchAndResult(0, Seq())
+
     if (isMatch) println(s"yes: ${result}") else println("no")
 }
