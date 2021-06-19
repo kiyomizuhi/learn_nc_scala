bddee70ac5552760cf142e4e1699cc14ccf165b7
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2021-06-19 15:36:00 +0900

    added a func to simple-search

---
+++ 00_intro/SimpleSearch.scala
@@ -0,0 +1,7 @@
+object SimpleSearch extends App {
+    val text = "bvaindabcvjiasuianvaomravranvfavabcniwu".toSeq
+    val pattern = "abc".toSeq
+    val matchIndexes = Seq()
+
+    println(s"found at ${matchIndexes}")
+}
