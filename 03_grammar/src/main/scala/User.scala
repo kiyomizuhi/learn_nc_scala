8451c0fa29ad11530c6f85ad3ea012a9504e71af
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2021-06-26 16:04:15 +0900

    modified to accept one string for the argument

--- 03_grammar/src/main/scala/User.scala
+++ 03_grammar/src/main/scala/User.scala
@@ -1,7 +1,16 @@
 class User(private val name: String, private val age: Int) {
   override def toString = s"User($name, $age)"
+
+  def printAge(user: User): Unit = println(user.age)
 }
 
 object User {
   def printAge(user: User): Unit = println(s"age is ${user.age}")
+
+  def apply(name_age: String): User = {
+    val res = name_age.split(",")
+    val name = res(0)
+    val age = res(1).toInt
+    new User(name, age)
+  }
 }
